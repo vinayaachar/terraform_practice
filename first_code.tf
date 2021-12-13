ef45297a1cd9356beb4d9422656a4a51dacb8eea
vinayaachar <vinayaachar@gmail.com>
2021-12-13 12:10:37 -0800

    Add Load Balance and 2 instances

--- first_code.tf
+++ first_code.tf
@@ -4,11 +4,25 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "prod_tf_course" {
-  bucket = "tf-course-20211212"
+  bucket = "tf-course2-20211212"
   acl    = "private"
 }
 
 resource "aws_default_vpc" "default" {}
+
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = "us-west-2a"
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
+resource "aws_default_subnet" "default_az2" {
+  availability_zone = "us-west-2b"
+  tags = {
+    "Terraform" : "true"
+  }
+} 
 
 resource "aws_security_group" "prod_web" {
   name        = "prod_web"
@@ -40,23 +54,47 @@ resource "aws_security_group" "prod_web" {
   }
 }
 
- resource "aws_instance" "prod_web" {
-   ami           = "ami-0f129ae03ee4c74a0"
-   instance_type = "t2.nano"
-   
-   vpc_security_group_ids = [
-   aws_security_group.prod_web.id
-  ]
+resource "aws_instance" "prod_web" {
+  count = 2
 
-   tags = {
-     "Terraform" : "true"
-   }	
+  ami           = "ami-0f129ae03ee4c74a0"
+  instance_type = "t2.nano"
+   
+  vpc_security_group_ids = [
+  aws_security_group.prod_web.id
+ ]
+
+  tags = {
+    "Terraform" : "true"
+  }	
+}
+
+resource "aws_eip_association" "prod_web" {
+  instance_id   = aws_instance.prod_web.0.id
+  allocation_id = aws_eip.prod_web.id
 }
 
 resource "aws_eip" "prod_web" {
-  instance = aws_instance.prod_web.id
+  tags = {
+    "Terraform" : "true"
+   }
+}
 
+
+resource "aws_elb" "prod_web" {
+  name      = "prod-web"
+  instances = aws_instance.prod_web.*.id
+  subnets   = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
+  security_groups = [aws_security_group.prod_web.id]
+  
+  listener {
+   instance_port     = 80
+   instance_protocol = "http"
+   lb_port           = 80
+   lb_protocol       = "http"
+  }
   tags = {
     "Terraform" : "true"
   }
 }
+
