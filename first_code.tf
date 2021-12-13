b4fe76e2a89d5b42e8ba4320786a07b294c5cc70
vinayaachar <vinayaachar@gmail.com>
2021-12-12 19:06:09 -0800

    Add security groups to AWS

--- first_code.tf
+++ first_code.tf
@@ -9,3 +9,33 @@ resource "aws_s3_bucket" "prod_tf_course" {
 }
 
 resource "aws_default_vpc" "default" {}
+
+resource "aws_security_group" "prod_web" {
+  name        = "prod_web"
+  description = "Allow standard http/https inbound/outbound"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["73.170.67.169/32"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["73.170.67.169/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["73.170.67.169/32"]
+  }
+  
+  tags = {
+    "Terraform" : "true"
+  }	
+}
