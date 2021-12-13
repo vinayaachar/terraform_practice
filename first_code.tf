1c393435cede426b86a0c46fb49f4a7d05464d23
vinayaachar <vinayaachar@gmail.com>
2021-12-13 13:07:52 -0800

    Add Auto Scaling groups to AWS

--- first_code.tf
+++ first_code.tf
@@ -54,37 +54,8 @@ resource "aws_security_group" "prod_web" {
   }
 }
 
-resource "aws_instance" "prod_web" {
-  count = 2
-
-  ami           = "ami-0f129ae03ee4c74a0"
-  instance_type = "t2.nano"
-   
-  vpc_security_group_ids = [
-  aws_security_group.prod_web.id
- ]
-
-  tags = {
-    "Terraform" : "true"
-  }	
-}
-
-resource "aws_eip_association" "prod_web" {
-  instance_id   = aws_instance.prod_web.0.id
-  allocation_id = aws_eip.prod_web.id
-}
-
-resource "aws_eip" "prod_web" {
-  tags = {
-    "Terraform" : "true"
-   }
-}
-
-
-resource "aws_elb" "prod_web" {
+resource "aws_elb" "prod_web" {   
   name      = "prod-web"
-  instances = aws_instance.prod_web.*.id
-  subnets   = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
   security_groups = [aws_security_group.prod_web.id]
   
   listener {
@@ -98,3 +69,33 @@ resource "aws_elb" "prod_web" {
   }
 }
 
+resource "aws_launch_template" "prod_web" {
+  name_prefix   = "prod-web"
+  image_id      = "ami-0f129ae03ee4c74a0"
+  instance_type = "t2.micro"
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
+resource "aws_autoscaling_group" "prod_web" {
+  availability_zones  = ["us-west-2a", "us-west-2b"]
+  desired_capacity    = 1
+  max_size            = 1
+  min_size            = 1
+
+  launch_template {
+    id      = aws_launch_template.prod_web.id
+    version = "$Latest"
+  }
+  tag {
+    key                 = "Terraform"
+    value               = "true"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_autoscaling_attachment" "prod_web" {
+  autoscaling_group_name = aws_autoscaling_group.prod_web.id
+  elb                    = aws_elb.prod_web.id
+}
